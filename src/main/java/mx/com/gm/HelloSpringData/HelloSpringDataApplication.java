a017111b73767af6bc9378370c9b14bd487d44b6
Patricia Rodríguez <prlongoria@gmail.com>
2022-11-06 19:50:37 +0100

    init project

---
+++ src/main/java/mx/com/gm/HelloSpringData/HelloSpringDataApplication.java
@@ -0,0 +1,13 @@
+package mx.com.gm.HelloSpringData;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloSpringDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloSpringDataApplication.class, args);
+	}
+
+}
---
+++ src/test/java/mx/com/gm/HelloSpringData/HelloSpringDataApplicationTests.java
@@ -0,0 +1,13 @@
+package mx.com.gm.HelloSpringData;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloSpringDataApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
